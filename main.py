31f5fd7aedf95b4cbab2ddf52a6bd0985ea0d4d3
haislanrafael <Haislanrafael1@gmail.com>
2025-03-31 21:59:30 -0300

    modificações-endpoint

--- main.py
+++ main.py
@@ -2,11 +2,11 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get("/")
+@app.get("/hellow word")
 async def root():
     return {"message": "hello word"}
 
-@app.get("/teste1")
+@app.get("/funcaoteste")
 async def funcaoteste():
-    return {"teste": "deu certo"}
+    return {"teste": "deu certo,haha"}
 
