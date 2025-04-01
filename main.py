485059b7fde48a20551b2f725239c560bb4eb815
haislanrafael <Haislanrafael1@gmail.com>
2025-03-31 21:35:52 -0300

    usando-fastapi

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print ("ola, mundo")
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+async def root():
+    return {"message": "hello word"}
+
+@app.get("/teste1")
+async def funcaoteste():
+    return {"teste": "deu certo"}
+
